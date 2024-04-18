6e31ba0a90e14c5943bb398686eee4ed7d2c7d6c
SirKonnor <144115654+SirKonnor@users.noreply.github.com>
2024-04-18 11:39:52 +0300

    Add files via upload

---
+++ site/java.js
@@ -0,0 +1,16 @@
+function getExhibits(museumID) {
+
+}
+
+function getStaff(museumID) {
+    
+}
+
+function showButtons() {
+    document.querySelector('.buttons').style.display = 'block';
+  }
+  
+  function hideButtons() {
+    document.querySelector('.buttons').style.display = 'none';
+  }
+  
